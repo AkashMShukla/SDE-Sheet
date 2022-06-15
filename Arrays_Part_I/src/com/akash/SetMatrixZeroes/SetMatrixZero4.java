622fd153fa1a255bacbc5e0c8dff1a3b39004762
Akash Shukla <akashshukla1442@gmail.com>
2022-06-15 22:14:37 +0530

    Done some changes & made new way to solve

---
+++ Arrays_Part_I/src/com/akash/SetMatrixZeroes/SetMatrixZero4.java
@@ -0,0 +1,44 @@
+// New solution using set
+package com.akash.SetMatrixZeroes;
+import java.util.HashSet;
+import java.util.Set;
+public class SetMatrixZero4 {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {0, 1, 2, 0},
+                {3, 4, 5, 2},
+                {1, 3, 1, 5}
+        };
+        setZeroes(arr);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void setZeroes(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+        Set<Integer> rows = new HashSet<Integer>();
+        Set<Integer> cols = new HashSet<Integer>();
+
+        // Essentially, we mark the rows and columns that are to be made zero
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+        // Iterate over the array once again and using the rows and cols sets, update the elements.
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
