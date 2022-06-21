d289bd9bcf55aea3f425cae8c0e58c686cd0eb2e
Akash Shukla <akashshukla1442@gmail.com>
2022-06-21 13:38:11 +0530

    Added swap function after if loop

--- Arrays_Part_I/src/com/akash/NextPermutation/NextPermutation2.java
+++ Arrays_Part_I/src/com/akash/NextPermutation/NextPermutation2.java
@@ -5,21 +5,22 @@ public class NextPermutation2 {
         int[] A = {1,3,5,4,2};
         System.out.println(Arrays.toString(Nextpermutation(A)));
     }
-    static int[] Nextpermutation(int[] A){
-        if(A == null || A.length <= 1){         //If the array is 1 or Null then return array itself
+    static int[] Nextpermutation(int[] A) {
+        if (A == null || A.length <= 1) {         //If the array is 1 or Null then return array itself
             return A;
         }
         int i = A.length - 2;       // We will keep i value to A.length - 2.
-        while(i >= 0 && A[i] >= A[i + 1]){  // We have to find the breakpoint i.e. A[i] > A[i+1]
+        while (i >= 0 && A[i] >= A[i + 1]) {  // We have to find the breakpoint i.e. A[i] > A[i+1]
             i--;
         }
-        if(i >= 0){
-            int j = A.length - 1;   // Keep j value to last value.
-            while(A[j] <= A[i]){    // we have to find the index where A[j] > A[i]
+        int j = 0;
+        if (i >= 0) {
+            j = A.length - 1;
+            while (A[j] <= A[i]) {    // we have to find the index where A[j] > A[i]
                 j--;
             }
-            swap(A,i,j);        // Swap the values present at i & j index.
         }
+        swap(A, i, j);                          // Swap the values present at i & j index.
         reverse(A, i + 1, A.length - 1);    // Reverse everything to the right of index i + 1
         return A;                               // We are returning array because we need to return array.
     }
