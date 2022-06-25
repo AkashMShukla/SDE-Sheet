ac601ea670b9d07b24534f7096f2171da0fe777a
Akash Shukla <akashshukla1442@gmail.com>
2022-06-25 13:12:08 +0530

    Leetcode You Tube Optimal Solution

---
+++ Arrays_Part_II/src/com/akash/MergeOverlappingSubintervals/MergeOverlapping1.java
@@ -0,0 +1,51 @@
+// Leetcode You Tube Solution. Optimal Solution
+package com.akash.MergeOverlappingSubintervals;
+import java.util.Arrays;
+import java.util.LinkedList;
+public class MergeOverlapping1 {
+    public static void main(String args[]) {
+        int[][] arr = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] ans = merge(arr);
+        System.out.println("Merged Overlapping Intervals are ");
+        for (int[] it: ans) {
+            System.out.println(it[0] + " " + it[1]);
+        }
+    }
+//    static ArrayList < List < Integer >> merge(ArrayList < List < Integer >> intervals) {
+//
+//        Collections.sort(intervals, (a,b) - > a.get(0) - b.get(0));
+//        ArrayList < List < Integer >> merged = new ArrayList < > ();
+//
+//        for (int i = 0; i < intervals.size(); i++) {
+//            if (merged.isEmpty() || merged.get(merged.size() - 1).get(1) < intervals.get(i).get(0)) {
+//                ArrayList < Integer > v = new ArrayList < > ();
+//                v.add(intervals.get(i).get(0));
+//                v.add(intervals.get(i).get(1));
+//
+//                merged.add(v);
+//            } else {
+//
+//                merged.get(merged.size() - 1).set(1, Math.max(merged.get(merged.size() - 1).get(1), intervals.get(i).get(1)));
+//            }
+//        }
+//
+//        return merged;
+//    }
+    static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
